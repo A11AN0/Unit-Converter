6f0858c5510f24bfaca3026195ea8122fbaee0bc
Allan <matrixboston7613@gmail.com>
2023-01-08 14:01:44 +0800

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Allan Odunga on 8/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
