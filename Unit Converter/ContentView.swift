517b2ba39e173c99b78fe58aefaeb49162113e30
Allan <matrixboston7613@gmail.com>
2023-01-09 08:10:27 +0800

    decimal keyboard with hide button incorporated

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var lengthInput = 0.0;
+    @FocusState private var inputIsFocused:Bool;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                
+                Section {
+                    TextField("Length to Convert", value: $lengthInput, format: .number)
+                    .keyboardType(.decimalPad)
+                    .focused($inputIsFocused)
+                } header: {
+                    Text("Input")
+                }
+                
+                Section {
+                    Text("random text")
+                } header: {
+                    Text("Output")
+                }
+            }
+            .navigationTitle("Unit Convert")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done") {
+                        inputIsFocused = false;
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
