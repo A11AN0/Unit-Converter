feea7db0b3cc92f27a66e5e5ce33bc82d7d30955
Allan <matrixboston7613@gmail.com>
2023-01-10 23:37:40 +0800

    segmented picker for units of measurement for input and output text fields

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -11,7 +11,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var lengthInput = 0.0;
+    @State private var inputMeasurementUnit = "m";
+    @State private var outputMeasurementUnit = "m";
     @FocusState private var inputIsFocused:Bool;
+    
+    private let measurementUnitList = ["m","mm","cm", "km", "mm", "in.", "ft", ]
     
     var body: some View {
         NavigationView {
@@ -21,14 +25,31 @@ struct ContentView: View {
                     TextField("Length to Convert", value: $lengthInput, format: .number)
                     .keyboardType(.decimalPad)
                     .focused($inputIsFocused)
+                    
+                    Picker("Input Units", selection: $inputMeasurementUnit) {
+                        ForEach(measurementUnitList, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    
+                    
                 } header: {
-                    Text("Input")
+                    Text("Input Length")
                 }
                 
                 Section {
-                    Text("random text")
+                    Text("\(lengthInput, format: .number)")
+                    
+                    Picker("Output Units", selection: $outputMeasurementUnit) {
+                        ForEach(measurementUnitList, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 } header: {
-                    Text("Output")
+                    Text("Output Length")
                 }
             }
             .navigationTitle("Unit Convert")
@@ -49,3 +70,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
