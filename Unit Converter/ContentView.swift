ab4657256b41dcba988a0505036c7ebf5662055e
Allan <matrixboston7613@gmail.com>
2023-01-11 19:53:48 +0800

    layout of app, and lengthConverter class created for basic conversion functions

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -13,46 +13,80 @@ struct ContentView: View {
     @State private var lengthInput = 0.0;
     @State private var inputMeasurementUnit = "m";
     @State private var outputMeasurementUnit = "m";
+    @State private var middleManValue = 4;
     @FocusState private var inputIsFocused:Bool;
     
-    private let measurementUnitList = ["m","mm","cm", "km", "mm", "in.", "ft", ]
+    private let measurementUnitList = LengthConverter.measurementUnitList
+    
+    //will make a class for length converter
+    class LengthConverter {
+        static public let measurementUnitList = ["m","mm","cm", "km","in.", "ft", "mi."];
+        
+    }
+    
     
     var body: some View {
         NavigationView {
-            Form {
-                
-                Section {
-                    TextField("Length to Convert", value: $lengthInput, format: .number)
-                    .keyboardType(.decimalPad)
-                    .focused($inputIsFocused)
+                Form {
                     
-                    Picker("Input Units", selection: $inputMeasurementUnit) {
-                        ForEach(measurementUnitList, id: \.self){
-                            Text($0)
+                    //required spacers To move contents to center of screen
+                    Section {
+                        VStack {
+                            ForEach(0..<7) { _ in
+                                Spacer()
+                            }
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .listRowBackground(Color.clear)
                     
+                    Section {
+                        TextField("Length to Convert", value: $lengthInput, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                        
+                        Picker("Input Units", selection: $inputMeasurementUnit) {
+                            ForEach(measurementUnitList, id: \.self){
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        
+                        
+                        
+                    } header: {
+                        Text("Input Length")
+
+                    }
                     
-                    
-                } header: {
-                    Text("Input Length")
-                }
-                
-                Section {
-                    Text("\(lengthInput, format: .number)")
-                    
-                    Picker("Output Units", selection: $outputMeasurementUnit) {
-                        ForEach(measurementUnitList, id: \.self){
-                            Text($0)
+                    Section {
+                        //need to horizontally align this
+                        HStack {
+                            Spacer()
+                            Image(systemName: "arrow.up.and.down")
+                                 .imageScale(.large)
+                                 .foregroundColor(.accentColor)
+                                 .font(.system(size: 50))
+                            Spacer()
+                            
+                            
                         }
                     }
-                    .pickerStyle(.segmented)
-                } header: {
-                    Text("Output Length")
+                    .listRowBackground(Color.clear)
+                    
+                    Section {
+                        Text("\(lengthInput, format: .number)")
+                        
+                        Picker("Output Units", selection: $outputMeasurementUnit) {
+                            ForEach(measurementUnitList, id: \.self){
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("Output Length")
+                    }
                 }
-            }
-            .navigationTitle("Unit Convert")
+            .navigationTitle("Unit Converter")
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard){
                     Spacer()
